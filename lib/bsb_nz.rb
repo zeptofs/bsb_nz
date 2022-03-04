085377311f0b73a40e20ad9deeb1f7bbeb23073c
Wei Zeng <gzzengwei@gmail.com>
2022-03-04 12:04:42 +1100

    bundle gem bsb_nz

---
+++ lib/bsb_nz.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "bsb_nz/version"
+
+module BsbNz
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bsb_nz/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module BsbNz
+  VERSION = "0.1.0"
+end
